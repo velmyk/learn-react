8967b93ed1a613029332a8d9c68098c4d1f3948a
velmyk <velmyk@gmail.com>
2016-06-30 13:46:22 +0300

    leaason #10 - colocating selectors with reducers

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,39 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import TodoList from './TodoList';
 import { toggleTodo } from '../actions';
-
-const getVisibleTodos = (
-	todos,
-	filter
-) => {
-	switch (filter) {
-		case 'all':
-			return todos;
-		case 'completed':
-			return todos.filter(t => t.completed);
-		case 'active':
-			return todos.filter(t => !t.completed);
-		default:
-			throw new Error(`Unknown filter: ${filter}.`);
-	}
-};
+import { getVisibleTodos } from '../reducers';
+import TodoList from './TodoList';
 
 const mapStateToTodoListProps = (state, { params }) => ({
 	todos: getVisibleTodos(
-		state.todos,
+		state,
 		params.filter || 'all'
 	)
 });
 
-const mapDispatchToTodoListProps = dispatch => ({
-	onTodoClick(id) { dispatch(toggleTodo(id)) }
-});
+// const mapDispatchToTodoListProps = dispatch => ({
+// 	onTodoClick(id) { dispatch(toggleTodo(id)) }
+// });
 
 const VisibleTodoList = withRouter(connect(
 	mapStateToTodoListProps,
-	mapDispatchToTodoListProps
+	{ onTodoClick: toggleTodo}
 )(TodoList));
 
 export default VisibleTodoList;
