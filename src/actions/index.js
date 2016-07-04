3236013667dc87f8c6d66a2449f46893198d284c
velmyk <velmyk@gmail.com>
2016-07-04 13:47:18 +0300

    lecture #26 - normalized response from server with normalizr

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { normalize } from 'normalizr';
+import * as schema from './schema';
 import { getIsFetching } from '../reducers';
 import * as api from '../api';
 
@@ -17,7 +19,7 @@ export const fetchTodos = filter => (dispatch, getState) => {
 			dispatch({
 				type: 'FETCH_TODOS_SUCCESS',
 				filter,
-				response
+				response: normalize(response, schema.arrayOfTodos)
 			}),
 		error =>
 			dispatch({
@@ -37,6 +39,6 @@ export const addTodo = text => dispatch =>
 	api.addTodo(text).then(response => {
 		dispatch({
 			type: 'ADD_TODO_SUCCESS',
-			response
+			response: normalize(response, schema.todo)
 		});
 	});
